ba662a94d757d7f3524248b257ce10cb3a30bfff
Pawan Dubey <40168397+p1web@users.noreply.github.com>
2025-09-11 11:36:04 +0530

    Update db.js
     Remove the semicolon after dbInstance

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -11,7 +11,7 @@ const dbName = "giftdb";
 async function connectToDatabase() {
     if (dbInstance){
         return dbInstance
-    };
+    }
 
     const client = new MongoClient(url, { useUnifiedTopology: true });   
 
@@ -23,7 +23,7 @@ async function connectToDatabase() {
     dbInstance = client.db(dbName);
 
     // Task 3: Return database instance
-     return dbInstance;
+     return dbInstance
 }
 
 module.exports = connectToDatabase;
